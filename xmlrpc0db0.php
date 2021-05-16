1aa45676a5201584ebcb4120df239099bae0cc7c
Ledfront <ledfront989@gmail.com>
2021-05-16 00:48:23 +0000

    First commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://whatdoyoumemetoken.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://whatdoyoumemetoken.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://whatdoyoumemetoken.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://whatdoyoumemetoken.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://whatdoyoumemetoken.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://whatdoyoumemetoken.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
